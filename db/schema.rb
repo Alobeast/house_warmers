45698c2869234fcf270627c28a165b472e299eae
amir yosef <amiryosef99@outlook.com>
2019-03-08 16:27:19 +0100

    edit front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_03_07_142910) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
