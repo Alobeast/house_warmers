e2208fea16641d34b1029828ada7e6fd5878f458
amir yosef <amiryosef99@outlook.com>
2019-03-11 17:24:42 +0100

    new front end changes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,8 +14,10 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    console.log(marker)
-    new mapboxgl.Marker()
+    const el = document.createElement('i');
+    el.className = 'fas fa-map-marker-alt fa-3x marker marker-' + marker.id;
+
+    new mapboxgl.Marker(el)
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
   });
@@ -24,7 +26,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 0, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 50, maxZoom: 15 });
 };
 
 const initMapbox = () => {
