8b8afe329a816db0b0daaff448db51800546ced2
amir yosef <amiryosef99@outlook.com>
2019-03-12 09:52:41 +0100

    colors

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -26,7 +26,8 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 50, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 50, maxZoom: 10 });
+  map.scrollZoom.disable();
 };
 
 const initMapbox = () => {
